98ccd9ee16377e6e48aa54046f1afd3b8f693867
hfischer <fischer@aladin.local>
2019-09-24 10:59:27 +0200

    Scripte-Ordner und Scripte dem Projekt hinzugefügt

---
+++ scripts/git-push.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+TIMESTAMP=`date +%Y%m%d_%H%M%S`
+EXTRACT_DIR="./.git_repo"
+
+find -L . \( -path "${EXTRACT_DIR}" -o -path "./database/temp" -o -path "./TEST" \) -prune -o -type d -exec mkdir -pv ${EXTRACT_DIR}/{} ';'
+
+for f in ` find -L . \( -path "${EXTRACT_DIR}" -o -path "./database/temp" -o -path "./TEST" \) -prune -o -type f -print `
+do
+
+DESTPATH=${EXTRACT_DIR}` echo ${f} | sed -e 's/^\.//g'`
+
+  case ` file ${f} | cut -d: -f2 | sed -e 's/^[[:space:]]*//' ` in
+    "OpenDocument Text" | "OpenDocument Text Template" | "OpenDocument Spreadsheet")
+      unzip -o -qq ${f} -d ${DESTPATH}
+
+      for g in ` find ${DESTPATH} -type f -name "content.xml" -o -name "meta.xml" -o -name "settings.xml" -o -name "styles.xml" `
+      do
+        mv ${g} "${g}_"
+        xmllint --format "${g}_" > ${g}
+        rm "${g}_"
+      done
+
+      ;;
+    *)
+      cp ${f} ${DESTPATH}
+      ;;
+  esac
+
+done
+
+pushd ${EXTRACT_DIR}
+  git add *
+  git commit
+  git push origin master
+popd
---
+++ scripts/pack-project.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DEST_DIR="../TEST"
+
+find . \( -name "*.odt" -o -name "*.ott" -o -name "*.ods" \) -prune -o -type d -exec mkdir -pv ${DEST_DIR}/{} ';'
+
+for f in ` find . \( -name "*.odt" -o -name "*.ott" -o -name "*.ods" \) -prune -o -type f -print `
+do
+  DESTPATH=${DEST_DIR}` echo ${f} | sed -e 's/^\.//g'`
+  cp ${f} ${DESTPATH}
+done
+
+
+for f in ` find . \( -name "*.odt" -o -name "*.ott" -o -name "*.ods" \) -a -type d -print `
+do
+  DESTPATH=${DEST_DIR}` echo ${f} | sed -e 's/^\.//g'`
+  pushd ${f}
+  zip -r "temp.zip" * --exclude "temp.zip"
+  popd
+  mv "${f}/temp.zip" ${DESTPATH}
+done
